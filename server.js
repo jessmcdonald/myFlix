c2e4102a9c344cd5ce9bfcd2e91b51741da9d3b3
jess <51135336+jessmcdonald@users.noreply.github.com>
2019-09-15 19:08:01 +0200

    First commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const http = require('http'),
+      url = require('url'),
+      fs = require('fs');
+
+http.createServer((request, response) => {
+  var addr = request.url,
+      q = url.parse(addr, true),
+      filePath = ' ';
+
+  if(q.pathname.includes('documentation')) {
+    filePath = (__dirname + '/documentation.html');
+  } else {
+    filePath = 'index.html';
+  }
+
+  fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + newDate() + '\n\n', function (err) {
+    if(err) {
+      console.log(err);
+    }
+      else {
+        console.log('Added to log');
+      }
+  });
+
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.end()
+}).listen(8080)
