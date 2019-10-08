9c8afec318f561317e17f8d0b2902818381f29b6
jess <51135336+jessmcdonald@users.noreply.github.com>
2019-10-08 12:51:39 +0200

    installed heroku, added start script & updated PORT

--- index.js
+++ index.js
@@ -269,5 +269,7 @@ app.delete('/users/:Username', passport.authenticate('jwt', { session: false }),
 });
 
 
-
-app.listen(8080);
+var port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0", function() {
+  console.log("Listening on Port 3000");
+});
