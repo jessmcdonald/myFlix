6f806f80e20b4fd091306d59641bed2366773bcf
Jess McDonald <51135336+jessmcdonald@users.noreply.github.com>
2019-10-10 17:20:41 +0200

    removed authentication logic from GET all movies

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.get('/', function (req, res) {
 
 //Gets all movie data
 
-app.get("/movies", passport.authenticate('jwt', { session: false }), function (req, res) {
+app.get("/movies", function (req, res) {
   Movies.find()
     .then(function (movies) {
       res.status(201).json(movies)
