7ec1def6545ab66929290ee10e3e076c123a7d24
Jess McDonald <51135336+jessmcdonald@users.noreply.github.com>
2019-10-11 11:27:56 +0200

    add localhost1234 to allowed origins in CORS policy

--- index.js
+++ index.js
@@ -15,24 +15,27 @@ require('./passport');
 
 var auth = require('./auth')(app);
 
-var allowedOrigins = ['http://localhost:8080', 'http://localhost:1234'];
-
 mongoose.connect('mongodb+srv://myFlixDBAdmin:MyFlixDB@sleepyjess-tftph.mongodb.net/myFlixDB?retryWrites=true&w=majority', { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false });
 //mongoose.connect('mongodb://localhost:27017/myFlixDB', { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false });
 
-//middleware functions
-app.use(express.static('public'));
-app.use(bodyParser.json());
+var allowedOrigins = ['http://localhost:8080', 'http://localhost:1234'];
+
 app.use(cors({
   origin: function (origin, callback) {
     if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1 {
-      var message = 'The CORS policy for this application does not allow access from origin' + origin;
+    if (allowedOrigins.indexOf(origin) === -1) {
+      var message = 'The CORS policy for this application does not allow access from origin ' + origin;
       return callback(new Error(message), false);
     }
     return callback(null, true);
   }
 }));
+
+
+//middleware functions
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
 
 app.use(function (err, req, res, next) {
   console.error(err.stack);
