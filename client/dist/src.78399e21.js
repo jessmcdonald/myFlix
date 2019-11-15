cae84b2f9a44c6b86b25bb6c640e459b27c15a84
jess <51135336+jessmcdonald@users.noreply.github.com>
2019-11-15 17:36:31 +0100

    cors fix for heroku + condition for slow loading data ProfileView

--- client/dist/src.78399e21.js
+++ client/dist/src.78399e21.js
@@ -57185,7 +57185,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function () { return cache; }; return cache; }
 
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; if (obj != null) { var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
 function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
 
@@ -57226,8 +57226,8 @@ function UpdateProfile(props) {
       birthday = _useState8[0],
       updateBirthday = _useState8[1];
 
-  var token = localStorage.getItem('token');
-  var user = localStorage.getItem('user');
+  var token = localStorage.getItem("token");
+  var user = localStorage.getItem("user");
 
   var handleUpdate = function handleUpdate(e) {
     e.preventDefault();
@@ -57243,11 +57243,11 @@ function UpdateProfile(props) {
       }
     }).then(function (response) {
       var data = response.data;
-      alert('Your information has been updated');
+      alert("Your information has been updated");
       console.log(data);
-      window.open('/userprofile', '_self');
+      window.open("/userprofile", "_self");
     }).catch(function (e) {
-      console.log('error updating user info');
+      console.log("error updating user info");
     });
   };
 
@@ -57259,17 +57259,17 @@ function UpdateProfile(props) {
         Authorization: "Bearer ".concat(token)
       }
     }).then(function (response) {
-      alert('Your account has been deleted');
-      localStorage.removeItem('token');
-      localStorage.removeItem('user');
-      window.open('/', '_self');
+      alert("Your account has been deleted");
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      window.open("/", "_self");
 
       _this.setState({
         user: null
       });
     }).catch(function (error) {
       console.log(error);
-      alert('Could not delete user info');
+      alert("Could not delete user info");
     });
   };
 
--- client/src/components/update-profile/update-profile.jsx
+++ client/src/components/update-profile/update-profile.jsx
@@ -1,124 +1,149 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-import './update-profile.scss';
+import "./update-profile.scss";
 
 export function UpdateProfile(props) {
-    const { userInfo } = props;
+  const { userInfo } = props;
 
-    if (!userInfo) {
-        return null;
-    }
+  if (!userInfo) {
+    return null;
+  }
 
-    console.log(userInfo);
+  console.log(userInfo);
 
-    const [username, updateUsername] = useState(userInfo.Username);
-    const [password, updatePassword] = useState(userInfo.Password);
-    const [email, updateEmail] = useState(userInfo.Email);
-    const [birthday, updateBirthday] = useState(userInfo.Birthday);
+  const [username, updateUsername] = useState(userInfo.Username);
+  const [password, updatePassword] = useState(userInfo.Password);
+  const [email, updateEmail] = useState(userInfo.Email);
+  const [birthday, updateBirthday] = useState(userInfo.Birthday);
 
-    let token = localStorage.getItem('token');
-    let user = localStorage.getItem('user');
+  let token = localStorage.getItem("token");
+  let user = localStorage.getItem("user");
 
-    const handleUpdate = (e) => {
-        e.preventDefault();
-        axios.put(`https://myflixmovies.herokuapp.com/users/${user}`, {
-            Username: username,
-            Password: password,
-            Email: email,
-            Birthday: birthday
-        }, {
-            headers: { Authorization: `Bearer ${token}` }
-        })
-            .then(response => {
-                const data = response.data;
-                alert('Your information has been updated');
-                console.log(data);
-                window.open('/userprofile', '_self');
-            })
-            .catch(e => {
-                console.log('error updating user info')
-            });
-    };
+  const handleUpdate = e => {
+    e.preventDefault();
+    axios
+      .put(
+        `https://myflixmovies.herokuapp.com/users/${user}`,
+        {
+          Username: username,
+          Password: password,
+          Email: email,
+          Birthday: birthday
+        },
+        {
+          headers: { Authorization: `Bearer ${token}` }
+        }
+      )
+      .then(response => {
+        const data = response.data;
+        alert("Your information has been updated");
+        console.log(data);
+        window.open("/userprofile", "_self");
+      })
+      .catch(e => {
+        console.log("error updating user info");
+      });
+  };
 
-    const handleDeregister = (e) => {
-        e.preventDefault();
+  const handleDeregister = e => {
+    e.preventDefault();
 
-        axios.delete(`https://myflixmovies.herokuapp.com/users/${user}`, {
-            headers: { Authorization: `Bearer ${token}` }
-        })
-            .then(response => {
-                alert('Your account has been deleted');
-                localStorage.removeItem('token');
-                localStorage.removeItem('user');
-                window.open('/', '_self');
-                this.setState({
-                    user: null
-                });
-            })
-            .catch(function (error) {
-                console.log(error);
-                alert('Could not delete user info')
-            });
-    }
+    axios
+      .delete(`https://myflixmovies.herokuapp.com/users/${user}`, {
+        headers: { Authorization: `Bearer ${token}` }
+      })
+      .then(response => {
+        alert("Your account has been deleted");
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
+        window.open("/", "_self");
+        this.setState({
+          user: null
+        });
+      })
+      .catch(function(error) {
+        console.log(error);
+        alert("Could not delete user info");
+      });
+  };
 
-    return (
-        <div className="profile-view" >
-            <div className="profile-page">
-                <Container className="profile-box">
-                    <Row>
-                        <Col>
-                            <div className="greeting">
-                                <h3>Hello {user}, you can edit your info below:</h3>
-                            </div>
-                            <Form>
-                                <Form.Group controlId="formGroupUsername">
-                                    <Form.Label>
-                                        Username:
-                                    <Form.Control plaintext readOnly defaultValue={user} />
-                                    </Form.Label>
-                                </Form.Group>
-                                <Form.Group>
-                                    <Form.Label>
-                                        Password:
-                                    <Form.Control type="password" value={password} onChange={e => updatePassword(e.target.value)} />
-                                    </Form.Label>
-                                </Form.Group>
-                                <Form.Group>
-                                    <Form.Label>
-                                        Email address:
-                                    <Form.Control type="email" value={email} onChange={e => updateEmail(e.target.value)} />
-                                    </Form.Label>
-                                </Form.Group>
-                                <Form.Group>
-                                    <Form.Label>
-                                        Birthday:
-                                    <Form.Control type="date" value={birthday} onChange={e => updateBirthday(e.target.value)} />
-                                    </Form.Label>
-                                </Form.Group>
-                                <Button className="edit-button" onClick={handleUpdate} >Save changes</Button>
-                            </Form >
-                            <div className="deregister-div">
-                                <h6>If you really want to leave us..</h6>
-                                <p className="deregister-div-warning">Warning: this is irreversible, all account information including your saved movies will be deleted.</p>
-                            </div>
-                            <div className="deregister-button-div">
-                                <Button className="deregister-button" variant="danger" onClick={handleDeregister}>Delete account</Button>
-                            </div>
-                        </Col>
-                    </Row>
-
-                </Container>
-            </div >
-        </div>
-    );
+  return (
+    <div className="profile-view">
+      <div className="profile-page">
+        <Container className="profile-box">
+          <Row>
+            <Col>
+              <div className="greeting">
+                <h3>Hello {user}, you can edit your info below:</h3>
+              </div>
+              <Form>
+                <Form.Group controlId="formGroupUsername">
+                  <Form.Label>
+                    Username:
+                    <Form.Control plaintext readOnly defaultValue={user} />
+                  </Form.Label>
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>
+                    Password:
+                    <Form.Control
+                      type="password"
+                      value={password}
+                      onChange={e => updatePassword(e.target.value)}
+                    />
+                  </Form.Label>
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>
+                    Email address:
+                    <Form.Control
+                      type="email"
+                      value={email}
+                      onChange={e => updateEmail(e.target.value)}
+                    />
+                  </Form.Label>
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>
+                    Birthday:
+                    <Form.Control
+                      type="date"
+                      value={birthday}
+                      onChange={e => updateBirthday(e.target.value)}
+                    />
+                  </Form.Label>
+                </Form.Group>
+                <Button className="edit-button" onClick={handleUpdate}>
+                  Save changes
+                </Button>
+              </Form>
+              <div className="deregister-div">
+                <h6>If you really want to leave us..</h6>
+                <p className="deregister-div-warning">
+                  Warning: this is irreversible, all account information
+                  including your saved movies will be deleted.
+                </p>
+              </div>
+              <div className="deregister-button-div">
+                <Button
+                  className="deregister-button"
+                  variant="danger"
+                  onClick={handleDeregister}
+                >
+                  Delete account
+                </Button>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
 }
-
-
-
